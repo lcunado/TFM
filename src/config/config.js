c6ea87565bddffd226c6a17cf569c200c1894046
[Laura Cuñado García] <[lcunado@uoc.edu]>
2025-10-27 01:24:58 +0100

    Agrego carga dinamica desde config

---
+++ src/config/config.js
@@ -0,0 +1,52 @@
+export const CONFIG = {
+    localidad: 'Burgos',
+    vivienda: "Refugio del Mirador",
+    direccion: "Calle San Pablo, 12<br>09002 Burgos<br>España",
+    latitud: 42.338405,
+    longitud: -3.700559,
+    telefono: "+34 600 123 456",
+    whatsapp: "+34 600 123 456",
+    email: "info@refugiodelmirador.com",
+    informacionGeneral: {
+        textos: [
+        'Ubicado en pleno corazón de Burgos, este alojamiento turístico ofrece una localización privilegiada, rodeado de los principales puntos de interés de la ciudad.',
+        'Con una superficie útil de 62 m², el apartamento está completamente equipado y orientado al Noreste y al Sur, lo que garantiza una excelente entrada de luz natural y una atmósfera cálida y confortable durante todo el año.',
+        'Entre sus comodidades destacan: conexión wifi de alta velocidad (500 Mb), smart TV, ascensor, calefacción, cocina totalmente equipada, salón con sofá cama, dos habitaciones con ventiladores de techo para invierno y verano, iluminación de tres ambientes, dos baños completos y plaza de garaje (altura máxima de vehículo: 2 m).',
+        'A escasa distancia encontrarás lugares emblemáticos como:',
+        'Además, en los alrededores hay tiendas, restaurantes y supermercados, incluyendo uno que permanece abierto todos los días de 9:00 a 21:30, incluso domingos y festivos.'
+        ],
+        lugares: [
+        'Catedral de Burgos (600 m)',
+        'Arco de Santa María (500 m)',
+        'Museo de la Evolución Humana (100 m)',
+        'Paseo del Espolón (300 m)',
+        'Plaza Mayor de Burgos (500 m)',
+        "Castillo de Burgos (1'1 km)"
+        ]
+    },
+    iconosIncluidos: [
+        { icono: "fa-house-user", texto: "Piso de 62 m²" },
+        { icono: "fa-users", texto: "1-5 personas" },
+        { icono: "fa-bed", texto: "2 habitaciones (una doble y una individual)" },
+        { icono: "fa-bath", texto: "2 baños completos" },
+        { icono: "fa-couch", texto: "Sofá cama" },
+        { icono: "fa-question", texto: "Totalmente equipado" },
+        { icono: "fa-wifi", texto: "Wi-Fi gratuito" },
+        { icono: "fa-tv", texto: "Smart TV" },
+        { icono: "fa-baby", texto: "Bebés 0-2 años gratis, disponen de cuna" },
+        { icono: "fa-temperature-high", texto: "Calefacción" },
+        { icono: "fa-square-parking", texto: "Garaje" }
+    ],
+    politicasReserva: {
+        iconos: [
+        { icono: "fa-clock", texto: "Entrada a partir de las 17:00 horas" },
+        { icono: "fa-clock", texto: "Salida hasta las 12:00 horas" }
+        ],
+        textos: [
+        "* Los horarios de entrada y salida pueden modificarse previa solicitud, siempre sujetos a disponibilidad.",
+        "Las reservas se formalizan mediante Bizum o transferencia bancaria. El pago debe realizarse en un plazo máximo de 24 horas tras la confirmación.",
+        "Todos los huéspedes deben estar debidamente registrados. Para ello, es necesario enviar el NIE, NIF o pasaporte de cada persona el día anterior a la entrada, con el fin de completar la firma a la llegada.",
+        "Política de cancelación: se reembolsa el 100% del importe si se cancela con al menos 7 días de antelación. En cancelaciones posteriores, se devuelve el 40%."
+        ]
+    }
+};
