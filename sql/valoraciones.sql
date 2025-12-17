5114e3b601bec191761386f9c3f32297ab18fa9a
[Laura Cuñado García] <[lcunado@uoc.edu]>
2025-12-17 13:09:41 +0100

    Corrijo sql valoraciones

--- sql/valoraciones.sql
+++ sql/valoraciones.sql
@@ -2,12 +2,12 @@ CREATE TABLE valoraciones (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     general TINYINT NOT NULL CHECK (general BETWEEN 1 AND 5),
-    limpieza TINYINT NOT NULL CHECK (limpieza BETWEEN 1 AND 5),
-    veracidad TINYINT NOT NULL CHECK (veracidad BETWEEN 1 AND 5),
-    llegada TINYINT NOT NULL CHECK (llegada BETWEEN 1 AND 5),
-    comunicacion TINYINT NOT NULL CHECK (comunicacion BETWEEN 1 AND 5),
-    ubicacion TINYINT NOT NULL CHECK (ubicacion BETWEEN 1 AND 5),
-    calidad TINYINT NOT NULL CHECK (calidad BETWEEN 1 AND 5),
+    limpieza TINYINT NULL CHECK (limpieza BETWEEN 1 AND 5),
+    veracidad TINYINT NULL CHECK (veracidad BETWEEN 1 AND 5),
+    llegada TINYINT NULL CHECK (llegada BETWEEN 1 AND 5),
+    comunicacion TINYINT NULL CHECK (comunicacion BETWEEN 1 AND 5),
+    ubicacion TINYINT NULL CHECK (ubicacion BETWEEN 1 AND 5),
+    calidad TINYINT NULL CHECK (calidad BETWEEN 1 AND 5),
     comentario TEXT,
     fecha_valoracion DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
